6abc77ad5e3d25d4e5b11c964b14badcf4adcb72
shin97 <shin9770@hotmail.com>
2018-01-04 19:48:41 +0800

    11

--- pr.js
+++ pr.js
@@ -5,118 +5,146 @@ prices.forEach( function(element) {
   total += element;
 } );
 
-document.querySelector('#checkout').addEventListener('click', () =>{
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
   if (!window.PaymentRequest) {
-
-    const msg = '此瀏覽器不支援Payment Request API!';
-    document.querySelector('#feedback').innerHTML = msg;
-    console.log(msg);
-    return;
+    return null;
   }
 
-  //支援付款方式
   const supportedInstruments = [{
-      supportedMethods: ['visa', 'mastercard', 'amex', 'discover','diners', 'jcb', 'unionpay']
-  },{
-    //整合Android Pay
     supportedMethods: ['https://android.com/pay'],
     data: {
-      //要在https://androidpay.developers.google.com/signup申請merchantId
-      merchantId: '02510116604241796260',
-      environment: 'TEST',
-      //Android Pay接受的信用卡種類
+      merchantName: 'Rouslan Solomakhin',
+      merchantId: '00184145120947117657',
       allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
       paymentMethodTokenizationParameters: {
         tokenizationType: 'GATEWAY_TOKEN',
         parameters: {
           'gateway': 'stripe',
-          //公用key
-          'stripe:publishableKey': 'pk_live_fD7ggZCtrB0vJNApRX5TyJ9T',
-          'stripe:version': '2016-07-06'
-        }
-      }
-    }
+          'stripe:publishableKey': 'pk_live_lNk21zqKM2BENZENh3rzCUgo',
+          'stripe:version': '2016-07-06',
+        },
+      },
+    },
+  }, {
+    supportedMethods: ['basic-card'],
   }];
 
-  //結帳細節
   const details = {
-    displayItems: [{
-      label: products[0],
-      amount: { currency: 'TWD', value: prices[0] }
-    }, {
-      label: products[1],
-      amount: { currency: 'TWD', value: prices[1] }
-    }, {
-      label: products[2],
-      amount: { currency: 'TWD', value: prices[2] }
-    }],
     total: {
       label: '總金額',
-      amount: { currency: 'TWD', value : total }
-    }
-  };
-
-  //設定要向用戶收取運費的電子郵件，地址和類型
-  const options = {
-    requestShipping: true,
-    requestPayerEmail: true
-  };
-
-  //使用之前收集的交易設定
-  //建立一個PaymentRequest
-  const request = new PaymentRequest(supportedInstruments, details, options);
-
-  //根據用戶所在的地區配置貨運類型
-  request.addEventListener('shippingaddresschange', function(evt) {
-    evt.updateWith(new Promise(function(resolve) {
-
-      const shippingOption = {
-        id: '',
-        label: '',
-        amount: {currency: 'TWD', value: '0'},
-        selected: true
-      };
-
-      if (request.shippingAddress.region === '桃園市') {
-        shippingOption.id = 'mg';
-        shippingOption.label = '免運費';
-        details.total.amount.value = '1';
-      } else {
-        shippingOption.id = 'world';
-        shippingOption.label = '快遞';
-        shippingOption.amount.value = '5';
-        details.total.amount.value = '6';
-      }
-
-      details.displayItems.splice(2, 1, shippingOption);
-      details.shippingOptions = [shippingOption];
-
-      resolve(details);
-    }));
-  });
-
-  //免運費最後通過show()方法顯示原生界面
-  request.show()
-  .then(result => {
-    //Demo: 將數據提交給伺服器
-    return fetch('/pay', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
+      amount: {
+        currency: 'TWD',
+        value: total,
       },
-      body: JSON.stringify(result.toJSON())
-    }).then(response => {
-      //查看付款狀態
-      if (response.status === 200) {
-        //付款成功
-        return result.complete('success');
-      } else {
-        //付款失敗
-        return result.complete('fail');
-      }
-    }).catch(() => {
-      return result.complete('fail');
-    });
-  });
-});
+    },
+    displayItems: [{
+      label: products[0],
+      amount: {
+        currency: 'TWD',
+        value: prices[0],
+      },
+    }, {
+      label: products[1],
+      amount: {
+        currency: 'TWD',
+        value: prices[1],
+      },
+    }],
+    modifiers: [{
+      supportedMethods: ['basic-card'],
+      data: {
+        supportedTypes: ['debit'],
+      },
+      total: {
+        label: 'Debit card discounted donation',
+        amount: {
+          currency: 'TWD',
+          value: '45.00',
+        },
+      },
+      additionalDisplayItems: [{
+        label: 'Debit card discount',
+        amount: {
+          currency: 'TWD',
+          value: '-10.00',
+        },
+      }],
+    }, {
+      supportedMethods: ['basic-card'],
+      data: {
+        supportedNetworks: ['mastercard'],
+      },
+      total: {
+        label: 'MasterCard discounted donation',
+        amount: {
+          currency: 'TWD',
+          value: '50.00',
+        },
+      },
+      additionalDisplayItems: [{
+        label: 'MasterCard discount',
+        amount: {
+          currency: 'TWD',
+          value: '-5.00',
+        },
+      }],
+    }],
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Launches payment request that does not require shipping.
+ */
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request.show()
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse.complete('success')
+            .then(function() {
+              done('This is a demo website. No payment will be processed.',
+                instrumentResponse);
+            })
+            .catch(function(err) {
+              error(err);
+              request = buildPaymentRequest();
+            });
+        }, 2000);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+    request = buildPaymentRequest();
+  }
+}
